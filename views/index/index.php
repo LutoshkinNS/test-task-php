10904fd53fcb97072a7a44e0676c6dad2cb62010
kashiuno <taigusiobaka@gmail.com>
2021-04-18 20:56:44 +0300

    Добавлены ссылки на детальные страницы продуктов на главной странице

--- views/index/index.php
+++ views/index/index.php
@@ -2,12 +2,17 @@
 /**
  * @var array $credits
  */
+
+use yii\helpers\Url;
+
 ?>
 <div class="display-4 mb-5">Общая информация</div>
 <div class="main-page-content d-flex">
 <?php foreach ($credits as $credit): ?>
     <div class="card hl-card mr-1 mb-1 p-3">
-        <div class="card-title"><?=$credit['name']?></div>
+        <a class="text-decoration-none" href="<?=Url::to("/credit/{$credit['id']}")?>">
+            <div class="card-title"><?=$credit['name']?></div>
+        </a>
         <div class="card-body text-info"><?=$credit['preview']?></div>
         <div class="d-flex hl-card-buttons">
             <button class="btn btn-dark">Пополнить</button>
